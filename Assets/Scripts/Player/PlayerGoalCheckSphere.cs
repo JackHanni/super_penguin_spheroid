817b9ad6cc735db5d211df47e5f3aa8c5e922a35
Braxton Owens <cbraxtonowens@gmail.com>
2024-03-27 14:59:51 -0600

    first commit

---
+++ Assets/Scripts/Player/PlayerGoalCheckSphere.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Checks if the player has passed through the goal
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  PlayerGoalCheckSphere : MonoBehaviour
+{
+    private PlayerController player;
+
+    private void Start()
+    {
+        player = transform.parent.GetComponent<PlayerController>(); // Get PlayerController component from parent
+    }
+
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("inside OnTriggerEnter()");
+        // Trigger collides with object tagged as ExitPortal and their current state is NORMAL
+        if (other.tag == "ExitPortal" && player.currentState == PlayerController.State.NORMAL)
+        {
+            Debug.Log("Player has reached the goal!");
+            player.currentState = PlayerController.State.VICTORY;   // Set player's current state to VICTORY state
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # super_penguin_spheroid
+
+# Goals
+- [ ] 6 Levels
+    - [ ] 1,3,6(Braxton)
+    - [ ] 2,4,5(Jack)
+
+- [ ] Looks Good ( ICE THEME ) 
+    - Gate texture(Braxton)
+    - Ball texture(Jack)
+    - Stage texure(Jack)
+        - might get complicated (werid shapes)
+    - Background(Braxton)
+
+- [ ] Main Menu(Jack)
+
+- [ ] Canvas(timer,score,etc)(Braxton) 
